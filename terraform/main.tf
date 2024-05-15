a4c6273c189fdecb0cfb09c097fede9c748cd652
Miles Phillips <1217784+millipz@users.noreply.github.com>
2024-05-15 10:52:06 +0100

    add default tags to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,17 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+  default_tags {
+    tags = {
+      ProjectName = "Deliverance Project"
+      Team = "Deliverance"
+      DeployedFrom = "Terraform"
+      Repository = "https://github.com/millipz/nc-de-deliverance-project"
+      CostCentre = "DE"
+      Environment = "dev"
+      RetentionDate = "2024-05-31"
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
