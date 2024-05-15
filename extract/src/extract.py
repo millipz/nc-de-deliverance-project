db9ddf89ed5f232f1bcbb85646e8b9f45d6aebbe
azmolmiah <azmol.miah.general@gmail.com>
2024-05-15 16:30:27 +0100

    fix flake8 issues with black

--- extract/src/extract.py
+++ extract/src/extract.py
@@ -1,17 +1,18 @@
 import boto3
 
+
 def retrieve_timestamps(table_name):
     try:
-        ssm_client = boto3.client('ssm')
-        
+        ssm_client = boto3.client("ssm")
+
         response = ssm_client.get_parameter(Name=table_name)
-        
-        timestamp = response['Parameter']['Value']
+
+        timestamp = response["Parameter"]["Value"]
         return timestamp
-    
+
     except ssm_client.exceptions.ParameterNotFound:
         raise KeyError(f"Table name '{table_name}' does not exist.")
-    
-    #TODO connection errors should be checked when connecting with credentials so this might not be needed here
+
+    # TODO connection errors should be checked when connecting with credentials so this might not be needed here
     # except (ssm_client.exceptions.ConnectionError):
     #     raise ConnectionError("Connection issue to Parameter Store.")
