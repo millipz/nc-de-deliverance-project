3ebe80e364b5b67bfd17b199a42f9f31a2904e97
Oliver Boyd <oliverjohnboyd@gmail.com>
2024-05-15 15:05:58 +0100

    completed make file and seeding of test db

--- db/run_seed.py
+++ db/run_seed.py
@@ -3,9 +3,6 @@ from db.connection import db
 from db.data.index import index as data
 
 try:
-    with open("db/schema.sql", 'r') as schema:
-        sql_schema = schema.read()
-    db.run(sql_schema)    
     seed(**data)
 except Exception as e:
     print(e)
