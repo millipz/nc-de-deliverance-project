46ded5b5b267dd098b4ff7eb732365d81b31f86d
Miles Phillips <1217784+millipz@users.noreply.github.com>
2024-05-15 10:55:15 +0100

    Create sample database in terraform

---
+++ terraform/test_db.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Allow incoming requests"
+  ingress {
+    description = "PostgreSQL ingress"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_db_instance" "totesys_test_db" {
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  storage_type           = "gp2"
+  engine_version         = "14.9"
+  db_name                = "totesys_sample_data"
+  username               = "postgres"
+  password               = "password"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
+
