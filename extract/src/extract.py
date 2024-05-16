2db9bbbb03bc0e74dfdb6ce960cfc87829e7bc73
Miles Phillips <1217784+millipz@users.noreply.github.com>
2024-05-16 11:51:27 +0100

    Lambda functions in progress

--- extract/src/extract.py
+++ extract/src/extract.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-def retrieve_timestamp(table_name: str, ssm_client) -> datetime:
+def get_timestamp(table_name: str, ssm_client) -> datetime:
     """
     Return timestamp showing most recent entry from the given table
     that has been processed by the ingestion lambda.
@@ -33,7 +33,7 @@ def retrieve_timestamp(table_name: str, ssm_client) -> datetime:
     #     raise ConnectionError("Connection issue to Parameter Store.")
 
 
-def write_timestamp(timestamp: datetime, table_name: str, ssm_client):
+def write_timestamp(timestamp: datetime, table_name: str, ssm_client) -> None:
     """
 
     Writes timestamp to parameter store for given table
@@ -60,7 +60,11 @@ def write_timestamp(timestamp: datetime, table_name: str, ssm_client):
         print(f"The timestamp could not be written: {e}")
 
 
-def collect_table_data(table_name: str, timestamp: datetime, db_conn):
+def collect_table_data(
+    table_name: str,
+    timestamp: datetime,
+    db_conn,
+) -> list[dict]:
     """
     Returns all data from a table newer than most recent timestamp
 
@@ -77,73 +81,81 @@ def collect_table_data(table_name: str, timestamp: datetime, db_conn):
     Returns:
         table_data (list) : list of dictionaries all data in table, one dictionary per row keys will be column headings
     """
+    pass
+
+
+def find_latest_timestamp(
+    table_data: list[dict],
+    columns=["last_updated"],
+) -> datetime:
+    """
+
+    Iterates over data from one database table and returns the most recent timestamp
+
+    Args:
+        table_data (list) : list of dictionaries representing rows of the table
+        columns (list[str], optional keyword) : columns to search for timestamps. Defauts to ["last_updated"]
+
+    Raises:
+        KeyError: columns do not exist
+
+    Returns:
+        most_recent_timestamp (timestamp) : from list returns most recent timestamp from created_at/updated_at values
+    """
+
+
+def write_table_data_to_s3(table_name: str, table_data: list[dict], s3_client) -> None:
+    """
+    Write file to S3 bucket as Json lines format
+
+    Args:
+        table_name (string)
+        table_data (list) : list of dictionaries all data in table, one dictionary per row keys will be column headings
+        s3_client (boto3 s3 client)
+
+
+    Raises:
+        FileExistsError: S3 object already exists with the same name
+        ConnectionError : connection issue to S3 bucket
+
+    Returns:
+        None
+    """
+
+
+def get_seq_id(table_name: str, ssm_client) -> int:
+    """
+    From parameter store retrieves table_name : sequential_id key value pair
+
+    Args:
+        table_name (string)
+
+
+    Raises:
+        KeyError: table_name does not exist
+        ConnectionError : connection issue to parameter store
+
+    Returns:
+        sequential_id(int)
 
     """
 
-        Collect data from one database table() returns the most recent timestamp
 
-        Args:
-            table_data (list) : list of dictionaries
-
-        Raises:
-            KeyError: created_at/updated_at does not exist
-
-
-
-        Returns:
-            most_recent_timestamp (timestamp) : from list returns most recent timestamp from created_at/updated_at values
-        """
-
+def write_seq_id(seq_id: int, table_name: str, ssm_client) -> None:
     """
 
-        Write file to S3 bucket as Json lines format
+    To parameter store write table_name : sequential_id key value pair
+    -- checks sequential_id is one greater than previous sequential_id
 
-        Args:
-            table_name (string)
-            most_recent_timestamp (timestamp) : timestamp of most recent records in data
-            table_data (list) : list of dictionaries all data in table, one dictionary per row keys will be column headings
-            sequential_id (int) : integer stored in parameter store retrieved earlier in application flow
+    Args:
+        table_name (string)
+        sequential_id(int)
 
 
-        Raises:
-            FileExistsError: S3 object already exists with the same name
-            ConnectionError : connection issue to S3 bucket
+    Raises:
+        KeyError: table_name does not exist
+        ConnectionError : connection issue to parameter store
 
-        Returns:
-            None
-        """
-
+    Returns:
+        None
     """
-
-        From parameter store retrieves table_name : sequential_id key value pair
-
-        Args:
-            table_name (string)
-
-
-        Raises:
-            KeyError: table_name does not exist
-            ConnectionError : connection issue to parameter store
-
-        Returns:
-            sequential_id(int)
-
-        """
-
-    """
-
-        To parameter store write table_name : sequential_id key value pair
-        -- checks sequential_id is one greater than previous sequential_id
-
-        Args:
-            table_name (string)
-            sequential_id(int)
-
-
-        Raises:
-            KeyError: table_name does not exist
-            ConnectionError : connection issue to parameter store
-
-        Returns:
-            None
-        """
--- extract/tests/test_extract.py
+++ extract/tests/test_extract.py
@@ -2,8 +2,14 @@ import boto3
 import os
 import pytest
 from moto import mock_aws
-from extract.src.extract import retrieve_timestamp, write_timestamp
+from extract.src.extract import (get_timestamp,
+                                 write_timestamp,
+                                 collect_table_data,
+                                 find_latest_timestamp,
+                                 write_table_data_to_s3,
+                                 get_seq_id, write_seq_id,)
 from datetime import datetime
+
 
 
 @pytest.fixture(scope="function")
@@ -21,11 +27,11 @@ def ssm_client(aws_creds):
         yield boto3.client("ssm")
 
 
-class TestRetrieveTimestamp:
+class TestGetTimestamp:
 
     def test_table_name_does_not_exist(self, ssm_client):
         with pytest.raises(KeyError):
-            retrieve_timestamp("non_existent_table", ssm_client)
+            get_timestamp("non_existent_table", ssm_client)
 
     def test_successful_retrieval(self, ssm_client):
         ssm_client.put_parameter(
@@ -41,8 +47,8 @@ class TestRetrieveTimestamp:
             Overwrite=True,
         )
 
-        # Retrieve the latest timestamp
-        timestamp = retrieve_timestamp("example_table", ssm_client)
+        # Get the latest timestamp
+        timestamp = get_timestamp("example_table", ssm_client)
         assert timestamp == "2024-05-16T10:50:30.123456"
 
     # TODO: Add a test for connection issues
@@ -58,5 +64,50 @@ class TestWriteTimestamp:
     def test_timestamp_writtem_to_param_store(self, ssm_client):
         time_to_write = datetime.fromisoformat("2024-05-16T10:50:30.123456")
         write_timestamp(time_to_write, "test_table", ssm_client)
-        timestamp = retrieve_timestamp("test_table", ssm_client)
+        timestamp = get_timestamp("test_table", ssm_client)
         assert str(timestamp) == "2024-05-16T10:50:30.123456"
+
+
+class TestCollectTableData:
+    
+    def test_returns_list_of_dicts(self):
+
+
+class TestFindLatestTimestamp:
+    pass
+
+class TestWriteTableDataToS3:
+    pass
+
+class TestGetSeqId:
+    def test_table_name_does_not_exist(self, ssm_client):
+        with pytest.raises(KeyError):
+            get_seq_id("non_existent_table", ssm_client)
+
+    def test_successful_retrieval(self, ssm_client):
+        ssm_client.put_parameter(
+            Name="example_table_latest_packet_id",
+            Value="00000100",
+            Type="String",
+        )
+
+        ssm_client.put_parameter(
+            Name="example_table_latest_packet_id",
+            Value="00000101",
+            Type="String",
+            Overwrite=True,
+        )
+
+        # Get the latest timestamp
+        id = get_seq_id("example_table", ssm_client)
+        assert id == 00000101
+
+    # TODO: Add a test for connection issues
+
+class TestWriteSeqId:
+
+    def test_id_writtem_to_param_store(self, ssm_client):
+        id_to_write = 00000101
+        write_timestamp(id_to_write, "test_table", ssm_client)
+        id = get_seq_id("test_table", ssm_client)
+        assert id == 00000101
