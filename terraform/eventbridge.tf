8ac3e3039236fefbe4e1fc3ac8e58c1807a5f750
Oliver Boyd <oliverjohnboyd@gmail.com>
2024-05-16 11:29:34 +0100

    add terraform configuration files for ingestion architecture

---
+++ terraform/eventbridge.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_event_rule" "ingestion_schedule" {
+    name = "ingestion_schedule"
+    description = "Eventbridge rule to trigger ingestion Lambda function every 10 minutes"
+    schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "ingestion_lambda_target" {
+    rule = aws_cloudwatch_event_rule.ingestion_schedule.name
+    target_id = "ingestion_lambda"
+    arn = aws_lambda_function.ingestion_function.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+    statement_id = "AllowExecutionFromEventBridge"
+    action = "lambda:InvokeFunction"
+    principal = "events.amazonaws.com"
+    function_name = aws_lambda_function.ingestion_function.function_name
+}
---
+++ terraform/iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Sid    = "",
+        Principal = {
+          Service = "lambda.amazonaws.com",
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_exec_policy" {
+  name   = "lambda_exec_policy"
+  role   = aws_iam_role.lambda_exec_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        Resource = "arn:aws:logs:*:*:*",
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+        ],
+        Resource = [
+          "arn:aws:s3:::nc-totesys-ingest",
+          "arn:aws:s3:::nc-totesys-ingest/*",
+        ],
+      },
+    ],
+  })
+}
