ac61d62efb33f2c573f08de6591f483035d1f052
Billy Hopkinson-Wood <billy@bhwood.dev>
2024-05-16 12:31:08 +0100

    add secrets manager terraform and variables to db connection details

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,33 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "totesys_username" {
+  description = "Database read-only username"
+  type        = string
+  sensitive   = true
+}
+
+variable "totesys_password" {
+  description = "totesys Database read-only password"
+  type        = string
+  sensitive   = true
+}
+
+variable "totesys_database" {
+  description = "Database name"
+  type        = string
+  sensitive   = true
+}
+
+variable "totesys_hostname" {
+  description = "totesys Database hostname"
+  type        = string
+  sensitive   = true
+}
+
+variable "totesys_port" {
+  description = "totesys Database port"
+  type        = string
+  sensitive   = true
+}
