6db9eca24417b32ca6e9e0e931a25e208bfbf657
Oliver Boyd <oliverjohnboyd@gmail.com>
2024-05-16 16:59:06 +0100

    work so far

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,5 +1,13 @@
 data "archive_file" "lambda_package" {
   type        = "zip"
-  source_file = "lambda_function.py"
-  output_path = "lambda_function.zip"
+  source_file = "${path.module}/../src/ingestion_function/lambda_function.py"
+  output_path = "${path.module}/../src/ingestion_function/lambda_function.zip"
+  output_file_mode = "0666"
+}
+
+data "archive_file" "layer_package" {
+  type        = "zip"
+  source_dir = "${path.module}/../layer/"
+  output_path = "${path.module}/../src/ingestion_function/layer.zip"
+  output_file_mode = "0666"
 }
