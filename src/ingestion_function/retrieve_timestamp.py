9fa1b54335abb32d9dcca330066e0af7c7a10070
Oliver Boyd <oliverjohnboyd@gmail.com>
2024-05-17 10:44:24 +0100

    work so far

--- src/ingestion_function/retrieve_timestamp.py
+++
@@ -1,5 +0,0 @@
-import boto3
-from botocore.exceptions import ClientError
-
-def retrieve_timestamp(table_name):
-    pass
--- terraform/data.tf
+++ terraform/data.tf
@@ -1,13 +1,13 @@
 data "archive_file" "lambda_package" {
   type        = "zip"
-  source_file = "${path.module}/../src/ingestion_function/lambda_function.py"
-  output_path = "${path.module}/../src/ingestion_function/lambda_function.zip"
+  source_file = "${path.module}/../src/lambda_function.py"
+  output_path = "${path.module}/../src/lambda_function.zip"
   output_file_mode = "0666"
 }
 
 data "archive_file" "layer_package" {
   type        = "zip"
   source_dir = "${path.module}/../layer/"
-  output_path = "${path.module}/../src/ingestion_function/layer.zip"
+  output_path = "${path.module}/../src/layer.zip"
   output_file_mode = "0666"
 }
