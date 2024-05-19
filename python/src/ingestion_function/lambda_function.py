e09afb69ae76a38e99b05a4b6320686cf07405f3
Oliver Boyd <oliverjohnboyd@gmail.com>
2024-05-19 18:50:00 +0100

    complete logging

--- python/src/ingestion_function/lambda_function.py
+++ python/src/ingestion_function/lambda_function.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import logging
 from datetime import datetime
 from pg8000.native import Connection
 from lambda_utils import (
@@ -15,10 +16,14 @@ from lambda_utils import (
 s3_client = boto3.client("s3")
 secrets_manager_client = boto3.client("secretsmanager")
 ssm_client = boto3.client("ssm")
-logs_client = boto3.client("logs")
 rds_client = boto3.client("rds")
+logs_client = boto3.client("logs")
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 S3_BUCKET = os.getenv("S3_BUCKET")
+
 DB_USERNAME = secrets_manager_client.get_secret_value(SecretId="totesys-username")[
     "SecretString"
 ]
@@ -55,34 +60,42 @@ db = Connection(
 
 
 def lambda_handler(event, context):
+    logger.info('## ENVIRONMENT VARIABLES')
+    logger.info(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
+    logger.info(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
+    logger.info('## EVENT')
+    logger.info(event)
+    
     for table in tables:
         try:
             timestamp = get_timestamp(table, ssm_client)
-            print(f"On last run the latest data from {table} was dated {timestamp}")
+            logger.info(f"On last run the latest data from {table} was dated {timestamp}")
         except KeyError:
             # assume first run, get all data
             timestamp = datetime.fromisoformat("2000-01-01")
-            print(f"No previous data logged from {table}")
+            logger.error(f"No previous data logged from {table}")
         data = collect_table_data(table, timestamp, db)
-        print(f"Data ingested for {table}")
+        logger.info(f"Data ingested for {table}")
         latest = find_latest_timestamp(data)
         try:
             last_id = get_seq_id(table, ssm_client)
         except KeyError:
             # assume first run
             last_id = 0
-            print("no previous runs logged for {table}")
+            logger.error("No previous runs logged for {table}")
         id = last_id + 1
-        print(f"this is run {id}")
+        logger.info(f"this is run {id}")
         try:
             write_table_data_to_s3(table, data, "nc-totesys-ingest", id, s3_client)
+            logger.info("Ingestion lambda successfully executed.")
         except Exception as e:
-            return {"statusCode": 500, "body": f"Error 500 Server Error: {e}"}
+            logger.error(f"Error writing {table} data to S3: {e}")
+            return {"statusCode": 500, "body": f"Error: {e}"}
         else:
             write_timestamp(latest, table, ssm_client)
             write_seq_id(id, table, ssm_client)
-            print(f"{table} data written to S3")
+            logger.info(f"{table} data written to S3")            
 
         # TODO - Invoke processing lambda
 
-    return {"statusCode": 200, "body": "Completed."}
+    return {"statusCode": 200, "body": "Ingestion completed successfully."}
--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -16,3 +16,36 @@ resource "aws_lambda_permission" "allow_eventbridge" {
     principal = "events.amazonaws.com"
     function_name = aws_lambda_function.ingestion_function.function_name
 }
+
+
+resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
+  alarm_name          = "lambda_error_alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 900
+  statistic           = "Sum"
+  threshold           = 2
+  alarm_description   = "Alarm when the Lambda function errors more than 2 times in a minute"
+  alarm_actions       = [aws_sns_topic.lambda_failure_topic.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.ingestion_function.name
+  }
+}
+
+resource "aws_sns_topic" "lambda_failure_topic" {
+  name = "lambda_failure_topic"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.lambda_failure_topic.arn
+  protocol  = "email"
+  endpoint  = "oliverjohnboyd@gmail.com"
+}
+
+resource "aws_cloudwatch_event_target" "send_sns" {
+  rule      = aws_cloudwatch_event_rule.alarm_state_change.name
+  arn       = aws_sns_topic.lambda_failure_topic.arn
+}
