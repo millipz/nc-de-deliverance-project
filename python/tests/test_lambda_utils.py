728c6d27e82e492f8fbc84910adf19bb8227ecba
Miles Phillips <1217784+millipz@users.noreply.github.com>
2024-05-20 10:33:18 +0100

    Revert environment changes

--- python/tests/test_lambda_utils.py
+++ python/tests/test_lambda_utils.py
@@ -21,16 +21,11 @@ from python.src.ingestion_function.lambda_utils import (
 
 @pytest.fixture(scope="function")
 def aws_creds():
-    try:
-        environment = os.environ["ENVIRONMENT"]
-    except KeyError:
-        environment = "TEST"
-    if environment == "DEV":
-        os.environ["AWS_ACCESS_KEY_ID"] = "test"
-        os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
-        os.environ["AWS_SECURITY_TOKEN"] = "test"
-        os.environ["AWS_SESSION_TOKEN"] = "test"
-        os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
     # TODO - edit to allow environment variables for dev/test/prod
 
