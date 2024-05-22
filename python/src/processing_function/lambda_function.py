dad9df4410f8d97ef039f7367f02021a4212686d
Miles Phillips <1217784+millipz@users.noreply.github.com>
2024-05-22 11:29:51 +0100

    Write docstrings and begin lambda function

--- python/src/processing_function/lambda_function.py
+++ python/src/processing_function/lambda_function.py
@@ -2,12 +2,20 @@ import boto3
 import os
 import logging
 from pg8000.native import Connection
-# from lambda_utils import (
-#     retrieve_data,
-#     transform_to_star_schema,
-#     get_packet_id,
-#     write_packet_id,
-# )
+from lambda_utils import (
+    retrieve_data,
+    transform_sales_order,
+    create_dim_date,
+    transform_staff,
+    transform_location,
+    transform_currency,
+    transform_design,
+    transform_counterparty,
+    transform_to_star_schema,
+    write_packet_id,
+    get_packet_id,
+    write_table_data_to_s3,
+)
 
 s3_client = boto3.client("s3")
 secrets_manager_client = boto3.client("secretsmanager")
@@ -19,22 +27,20 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 S3_BUCKET = os.getenv("S3_BUCKET")
+ENVIRONMENT = os.getenv("ENVIRONMENT")
 
-DB_USERNAME = secrets_manager_client.get_secret_value(SecretId="totesys-username")[
-    "SecretString"
-]
-DB_PASSWORD = secrets_manager_client.get_secret_value(SecretId="totesys-password")[
-    "SecretString"
-]
-DB_HOST = secrets_manager_client.get_secret_value(SecretId="totesys-hostname")[
-    "SecretString"
-]
-DB_PORT = secrets_manager_client.get_secret_value(SecretId="totesys-port")[
-    "SecretString"
-]
-DB_NAME = secrets_manager_client.get_secret_value(SecretId="totesys-database")[
-    "SecretString"
-]
+DB_USERNAME = secrets_manager_client.get_secret_value(
+    SecretId=f"totesys_{ENVIRONMENT}_db_username"
+)["SecretString"]
+DB_PASSWORD = secrets_manager_client.get_secret_value(
+    SecretId=f"totesys_{ENVIRONMENT}_db_password"
+)["SecretString"]
+DB_HOST, DB_PORT = secrets_manager_client.get_secret_value(
+    SecretId=f"totesys_{ENVIRONMENT}_db_endpoint"
+)["SecretString"].split(":")
+DB_NAME = secrets_manager_client.get_secret_value(
+    SecretId=f"totesys_{ENVIRONMENT}_db_name"
+)["SecretString"]
 
 tables = [
     "address",
@@ -55,10 +61,50 @@ db = Connection(
 )
 
 
-# def lambda_handler(event, context):
-#     logger.info('## ENVIRONMENT VARIABLES')
-#     logger.info(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
-#     logger.info(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
-#     logger.info('## EVENT')
-#     logger.info(event)
-#     for table in tables:
+def lambda_handler(event, context):
+    logger.info("## ENVIRONMENT VARIABLES")
+    logger.info(os.environ["AWS_LAMBDA_LOG_GROUP_NAME"])
+    logger.info(os.environ["AWS_LAMBDA_LOG_STREAM_NAME"])
+    logger.info("## EVENT")
+    logger.info(event)
+
+    response_data = {}
+    total_ingested_rows = 0
+
+    for table in tables:
+        try:
+            timestamp = get_timestamp(ENVIRONMENT + "_" + table, ssm_client)
+            logger.info(
+                f"On last run the latest data from {table} was dated {timestamp}"
+            )
+        except KeyError:
+            # assume first run, get all data
+            timestamp = datetime.fromisoformat("2000-01-01")
+            logger.error(f"No previous data logged from {table}")
+        data = collect_table_data(table, timestamp, db)
+        total_ingested_rows += len(data)
+        if len(data) == 0:
+            logger.info(f"No new data for {table}")
+        else:
+            logger.info(f"Data ingested for {table}")
+            latest = find_latest_timestamp(data)
+            try:
+                last_id = get_seq_id(ENVIRONMENT + "_" + table, ssm_client)
+            except KeyError:
+                # assume first run
+                last_id = 0
+                logger.error("No previous runs logged for {table}")
+            id = last_id + 1
+            logger.info(f"this is run {id}")
+            try:
+                key = write_table_data_to_s3(table, data, S3_BUCKET, id, s3_client)
+            except Exception as e:
+                logger.error(f"Error writing {table} data to S3: {e}")
+                return {"statusCode": 500, "body": f"Error: {e}"}
+            else:
+                write_timestamp(latest, ENVIRONMENT + "_" + table, ssm_client)
+                write_seq_id(id, ENVIRONMENT + "_" + table, ssm_client)
+                logger.info(f"{table} data written to S3, {len(data)} rows ingested")
+                response_data[table] = key
+    logger.info(f"{total_ingested_rows} rows ingested this run")
+    return {"statusCode": 200, "data": response_data}
---
+++ python/src/processing_function/lambda_utils_docstrings.py
@@ -0,0 +1,282 @@
+import pandas as pd
+import json
+
+
+def retrieve_data(bucket_name: str, object_key: str, s3_client):
+    """
+    Load data from an s3 object (json lines) into a pandas dataframe
+
+    Args:
+        bucket_name (str): bucket where the object is stored
+        object_key (str): key of data object
+        client (boto3 s3 Client)
+
+    Raises:
+        KeyError: table_name does not exist
+        ConnectionError : connection issue to parameter store
+
+    Returns:
+        data: pandas dataframe
+    """
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+        data = json.loads(response["Body"].read().decode('utf-8'))
+        df = pd.DataFrame(data)
+        return df
+
+    except s3_client.exceptions.NoSuchKey:
+        raise KeyError(f"The key '{object_key}' does not exist.")
+
+
+# def transform_to_star_schema(sales_order_df, staff_df,
+# address_df, currency_df, design_df, counterparty_df):
+
+
+def transform_sales_order(sales_order_df):
+    """
+    Transform loaded sales order data into star schema
+        - splits out time and date data into seperate columns
+        - renames staff_id to sales_staff_id
+        - removes unwanted columns
+
+    Args:
+        sales_order_df (pandas dataframe): original data
+
+    Returns:
+        data (pandas dataframe): transformed data
+    """
+    fact_sales_order = sales_order_df.copy()
+
+    fact_sales_order['created_date'] = pd.to_datetime(fact_sales_order['created_at']).dt.date
+    fact_sales_order['created_time'] = pd.to_datetime(fact_sales_order['created_at']).dt.time
+    fact_sales_order['last_updated_date'] = \
+        pd.to_datetime(fact_sales_order['last_updated']).dt.date
+    fact_sales_order['last_updated_time'] = \
+        pd.to_datetime(fact_sales_order['last_updated']).dt.time
+
+    fact_sales_order.rename(columns={
+        'staff_id': 'sales_staff_id',
+    }, inplace=True)
+
+    fact_sales_order = fact_sales_order[[
+        'sales_order_id',
+        'created_date',
+        'created_time',
+        'last_updated_date',
+        'last_updated_time',
+        'sales_staff_id',
+        'counterparty_id',
+        'units_sold',
+        'unit_price',
+        'currency_id',
+        'design_id',
+        'agreed_payment_date',
+        'agreed_delivery_date',
+        'agreed_delivery_location_id']]
+
+    return fact_sales_order
+
+
+def create_dim_date(start_date, end_date):
+    """
+    Creates a table of dates in the given range
+    with columns for:
+        - year
+        - month
+        - month name
+        - day of month
+        - day of year
+        - day of week
+        - quarter
+
+
+    Args:
+        start_date (datetime)
+        end_date (datetime)
+
+    Returns:
+        dates (pandas dataframe)
+    """
+    date_range = pd.date_range(start=start_date, end=end_date)
+    dim_date = pd.DataFrame(date_range, columns=['date_id'])
+    dim_date['year'] = dim_date['date_id'].dt.year
+    dim_date['month'] = dim_date['date_id'].dt.month
+    dim_date['day'] = dim_date['date_id'].dt.day
+    dim_date['day_of_week'] = dim_date['date_id'].dt.weekday
+    dim_date['day_name'] = dim_date['date_id'].dt.day_name()
+    dim_date['month_name'] = dim_date['date_id'].dt.month_name()
+    dim_date['quarter'] = dim_date['date_id'].dt.quarter
+    return dim_date
+
+
+def transform_staff(staff_df, department_df):
+    """
+    Transform loaded staff order data into star schema
+        - adds a location by reference to department table
+
+    Args:
+        staff_df (pandas dataframe): original data
+        department_df (pandas dataframe): department data for location
+
+    Returns:
+        data (pandas dataframe): transformed data
+    """
+    dim_staff = staff_df.copy()
+    dim_staff = dim_staff.merge(department_df[['department_id', 'department_name', 'location']],
+                                on='department_id', how='left')
+    return dim_staff
+
+
+def transform_location(address_df):
+    """
+    Transform loaded address order data into star schema
+        - renames address_id to location_id
+
+    Args:
+        address_df (pandas dataframe): original data
+
+    Returns:
+        data (pandas dataframe): transformed data
+    """
+    dim_location = address_df.copy()
+    dim_location = dim_location.rename(columns={
+        'address_id': 'location_id'    
+        })
+    return dim_location
+
+
+def transform_currency(currency_df):
+    """
+    Transform loaded currency data into star schema
+        - renames address_id to location_id
+
+    Args:
+        address_df (pandas dataframe): original data
+
+    Returns:
+        data (pandas dataframe): transformed data
+    """
+    dim_currency = currency_df.copy()
+    dim_currency['currency_name'] = dim_currency['currency_code'].apply(lambda x: 'Unknown')
+    return dim_currency
+
+
+def transform_design(design_df):
+    dim_design = design_df.copy()
+    return dim_design[[
+        'design_id', 'design_name', 'file_location', 'file_name'
+    ]]
+
+
+def transform_counterparty(counterparty_df, address_df):
+    dim_counterparty = counterparty_df.copy()
+    dim_counterparty = dim_counterparty.merge(address_df, left_on='legal_address_id',
+                                              right_on='address_id', how='left')
+    dim_counterparty = dim_counterparty.rename(columns={
+        'counterparty_id': 'counterparty_id',
+        'counterparty_legal_name': 'counterparty_legal_name',
+        'address_line_1': 'counterparty_legal_address_line_1',
+        'address_line_2': 'counterparty_legal_address_line_2',
+        'district': 'counterparty_legal_district',
+        'city': 'counterparty_legal_city',
+        'postal_code': 'counterparty_legal_postal_code',
+        'country': 'counterparty_legal_country',
+        'phone': 'counterparty_legal_phone_number'
+    })
+    return dim_counterparty
+
+
+def transform_to_star_schema(sales_order_df, staff_df, address_df, currency_df,
+                             design_df, counterparty_df, department_df):
+    fact_sales_order = transform_sales_order(sales_order_df)
+    dim_date = create_dim_date(fact_sales_order)
+    dim_staff = transform_staff(staff_df, department_df)
+    dim_location = transform_location(address_df)
+    dim_currency = transform_currency(currency_df)
+    dim_design = transform_design(design_df)
+    dim_counterparty = transform_counterparty(counterparty_df, address_df)
+    return fact_sales_order, dim_date, dim_staff, dim_location, \
+        dim_currency, dim_design, dim_counterparty
+
+
+def write_packet_id(packet_id: int, table_name: str, ssm_client) -> None:
+    """
+
+    Write table_name : packet_id key value pair to Parameter Store
+
+    Args:
+        table_name (string)
+        packet_id(int)
+
+    Raises:
+        KeyError: table_name does not exist
+        ConnectionError : connection issue to parameter store
+
+    Returns:
+        None
+    """
+    try:
+        print("writing ")
+        ssm_client.put_parameter(
+            Name=f"/processing/{table_name}/latest_packet_id",
+            Type="String",
+            Description=(
+                "Latest packet_id of data processed "
+                f"from Totesys database for {table_name} table"
+            ),
+            Value=str(packet_id),
+            Overwrite=True,
+        )
+    except Exception as e:
+        print(f"The packet ID could not be written: {e}")
+
+
+def get_packet_id(table_name: str, ssm_client) -> int:
+    """
+    From parameter store retrieves table_name : packet_id key value pair
+
+    Args:
+        table_name (string)
+
+
+    Raises:
+        KeyError: table_name does not exist
+        ConnectionError : connection issue to parameter store
+
+    Returns:
+        packet_id(int)
+
+    """
+    try:
+        response = ssm_client.get_parameter(Name=("/processing/"+table_name+"/latest_packet_id"))
+        id = response["Parameter"]["Value"]
+        return int(id)
+
+    except ssm_client.exceptions.ParameterNotFound:
+        raise KeyError(f"Table name '{table_name}' does not have any recorded packets.")
+
+
+# def write_table_data_to_s3(
+#     dict_name dict,
+#     bucket_name: str,
+#     packet_id: int,
+#     s3_client,
+# ) -> None:
+# """
+# Write file to S3 bucket in Parquet format
+
+# Args:
+#     table_name (string)
+#     table_data (list) : list of dictionaries all data in table,
+#         one dictionary per row keys will be column headings
+#     bucket_name (string)
+#     packet_id (string)
+#     s3_client (boto3 s3 client)
+
+# Raises:
+#     FileExistsError: S3 object already exists with the same name
+#     ConnectionError : connection issue to S3 bucket
+
+# Returns:
+#     None
+# """
