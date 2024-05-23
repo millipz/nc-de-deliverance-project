cfa37cbc75563068c36412c933cc3525f7d54cf3
Oliver Boyd <oliverjohnboyd@gmail.com>
2024-05-23 11:56:35 +0100

    fix bug in object key split statement

--- python/src/processing_function/lambda_function.py
+++ python/src/processing_function/lambda_function.py
@@ -93,7 +93,7 @@ def lambda_handler(event, context):
             case _:
                 logger.error(f"Unknown table name: {table_name}")
                 continue
-        packet_id = int(object_key.split("_")[1])
+        packet_id = int(object_key.split("_")[-2])
         processed_key = write_data_to_s3(processed_data_frames[new_table_name], new_table_name,
                                          S3_PROCESSED_BUCKET, packet_id, s3_client)
         response_data[new_table_name] = processed_key
