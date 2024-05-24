2e8da71c5ce1a06b7b271da1cf78ccb8225d0e0b
Oliver Boyd <oliverjohnboyd@gmail.com>
2024-05-24 16:39:53 +0100

    some work on processing

--- python/tests/ingestion_function/test_ingestion_lambda.py
+++ python/tests/ingestion_function/test_ingestion_lambda.py
@@ -2,7 +2,7 @@ import os
 import boto3
 import pytest
 from moto import mock_aws
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from dotenv import load_dotenv
 
 from python.src.ingestion_function.lambda_function import lambda_handler, tables
@@ -14,7 +14,6 @@ db_username = os.getenv("TEST_DB_USERNAME")
 db_password = os.getenv("TEST_DB_PASSWORD")
 
 
-# Fixtures for AWS clients
 @pytest.fixture(scope="function")
 def aws_credentials():
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
@@ -22,13 +21,6 @@ def aws_credentials():
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
-
-
-@pytest.fixture(scope="function")
-def s3_client(aws_credentials):
-    """Create an S3 client using moto for mocking."""
-    with mock_aws():
-        yield boto3.client("s3", region_name="eu-west-2")
 
 
 @pytest.fixture(scope="function")
@@ -42,14 +34,6 @@ def secrets_manager_client(aws_credentials):
         yield client
 
 
-@pytest.fixture(scope="function")
-def ssm_client(aws_credentials):
-    """Create an SSM client using moto for mocking."""
-    with mock_aws():
-        yield boto3.client("ssm", region_name="eu-west-2")
-
-
-# Fixtures for environment and event/context
 @pytest.fixture(scope="function")
 def lambda_event():
     return {"key1": "value1", "key2": "value2", "key3": "value3"}
@@ -88,9 +72,6 @@ def test_lambda_handler(
     mock_collect_table_data,
     mock_write_timestamp,
     mock_get_timestamp,
-    s3_client,
-    secrets_manager_client,
-    ssm_client,
     lambda_event,
     lambda_context,
 ):
@@ -110,9 +91,6 @@ def test_lambda_handler(
         mock_collect_table_data: A mocked function for collecting table data.
         mock_write_timestamp: A mocked function for writing timestamps.
         mock_get_timestamp: A mocked function for getting timestamps.
-        s3_client: A mocked S3 client.
-        secrets_manager_client: A mocked Secrets Manager client.
-        ssm_client: A mocked SSM client.
         lambda_event: The sample Lambda event.
         lambda_context: The mocked Lambda context.
     """
@@ -120,8 +98,6 @@ def test_lambda_handler(
     os.environ["S3_BUCKET"] = "my-bucket"
     os.environ["AWS_LAMBDA_LOG_GROUP_NAME"] = "log-group"
     os.environ["AWS_LAMBDA_LOG_STREAM_NAME"] = "log-stream"
-
-    mock_pg_connection.return_value = MagicMock()
 
     mock_get_timestamp.return_value = "2000-01-01T00:00:00"
     mock_collect_table_data.return_value = [
